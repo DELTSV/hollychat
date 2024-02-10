31a12094d638a07cd2776aebbb87316122f4ccba
Loïc Vanden Bossche <loloic066@gmail.com>
2024-02-10 17:59:38 +0100

    Fixed inkwell clickable when loading

--- lib/posts/widgets/image_viewer.dart
+++ lib/posts/widgets/image_viewer.dart
@@ -5,7 +5,7 @@ import 'package:hollychat/models/post_image.dart';
 
 import '../image_screen/image_screen.dart';
 
-class ImageViewer extends StatelessWidget {
+class ImageViewer extends StatefulWidget {
   const ImageViewer({
     super.key,
     required this.postImage,
@@ -14,18 +14,25 @@ class ImageViewer extends StatelessWidget {
   final PostImage postImage;
 
   @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  bool isImageLoaded = false;
+
+  @override
   Widget build(BuildContext context) {
     final UniqueKey tag = UniqueKey();
 
     final image = LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         double width = constraints.maxWidth;
-        final aspectRatio = postImage.width / postImage.height;
+        final aspectRatio = widget.postImage.width / widget.postImage.height;
 
         final height = width / aspectRatio;
 
         return Image.network(
-          postImage.url,
+          widget.postImage.url,
           height: height,
           width: width,
           loadingBuilder: (
@@ -35,6 +42,16 @@ class ImageViewer extends StatelessWidget {
           ) {
             if (loadingProgress == null) {
               return child;
+            }
+
+            // check if the image is loaded
+            if (loadingProgress.cumulativeBytesLoaded ==
+                loadingProgress.expectedTotalBytes) {
+              WidgetsBinding.instance.addPostFrameCallback(
+                (_) => setState(() {
+                  isImageLoaded = true;
+                }),
+              );
             }
 
             return SizedBox(
@@ -59,32 +76,38 @@ class ImageViewer extends StatelessWidget {
       child: Stack(
         children: <Widget>[
           image,
-          Positioned.fill(
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: () {
-                  Future.delayed(const Duration(milliseconds: 200)).then(
-                    (_) {
-                      Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                          opaque: false,
-                          barrierColor: Colors.white.withOpacity(0),
-                          pageBuilder: (BuildContext context, _, __) {
-                            return ImageScreen(
-                              tag: tag,
-                              child: image,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                  );
-                },
+          Builder(builder: (context) {
+            if (!isImageLoaded) {
+              return const SizedBox();
+            }
+
+            return Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Future.delayed(const Duration(milliseconds: 200)).then(
+                      (_) {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            opaque: false,
+                            barrierColor: Colors.white.withOpacity(0),
+                            pageBuilder: (BuildContext context, _, __) {
+                              return ImageScreen(
+                                tag: tag,
+                                child: image,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ),
+            );
+          }),
         ],
       ),
     );
