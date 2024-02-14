dd19222b2240bc9da5adf3547d0cefa0fe9c1b72
MacaronFR <denisft77@gmail.com>
2024-02-14 23:54:55 +0100

    No code duplication

--- lib/models/link_preview.dart
+++ lib/models/link_preview.dart
@@ -1,3 +1,8 @@
+import 'package:collection/collection.dart';
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart';
+
 class LinkPreview {
   final String url;
   final String title;
@@ -12,4 +17,38 @@ class LinkPreview {
     required this.image,
     required this.siteName
   });
+
+  static Future<LinkPreview?> getFromUrl(String url) async {
+    var response = await get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      return null;
+    }
+
+    var doc = parse(response.body);
+    Map linkData = {};
+    _getOG(doc, linkData, 'og:title');
+    _getOG(doc, linkData, 'og:description');
+    _getOG(doc, linkData, 'og:site_name');
+    _getOG(doc, linkData, 'og:image');
+
+    if (linkData.isNotEmpty && linkData.containsKey("og:title") &&
+        linkData.containsKey("og:site_name") &&
+        linkData.containsKey("og:description")) {
+      return LinkPreview(url: url,
+        title: linkData['og:title'],
+        description: linkData['og:description'],
+        image: linkData['og:image'],
+        siteName: linkData['og:site_name'],);
+    } else {
+      return null;
+    }
+  }
+
+  static void _getOG(Document doc, Map data, String param) {
+    var metaTag = doc.getElementsByTagName("meta").firstWhereOrNull((
+        meta) => meta.attributes['property'] == param);
+    if (metaTag != null) {
+      data[param] = metaTag.attributes['content'];
+    }
+  }
 }
--- lib/models/post.dart
+++ lib/models/post.dart
@@ -78,42 +78,8 @@ class Post {
 
   Future<List<LinkPreview>> getPreviews() async {
     var a = links.map((url) =>
-        getPreview(url)
+        LinkPreview.getFromUrl(url)
     );
     return (await Future.wait(a)).whereType<LinkPreview>().toList();
-  }
-
-  Future<LinkPreview?> getPreview(String url) async {
-    var response = await get(Uri.parse(url));
-    if (response.statusCode != 200) {
-      return null;
-    }
-
-    var doc = parse(response.body);
-    Map linkData = {};
-    _getOG(doc, linkData, 'og:title');
-    _getOG(doc, linkData, 'og:description');
-    _getOG(doc, linkData, 'og:site_name');
-    _getOG(doc, linkData, 'og:image');
-
-    if (linkData.isNotEmpty && linkData.containsKey("og:title") &&
-        linkData.containsKey("og:site_name") &&
-        linkData.containsKey("og:description")) {
-      return LinkPreview(url: url,
-        title: linkData['og:title'],
-        description: linkData['og:description'],
-        image: linkData['og:image'],
-        siteName: linkData['og:site_name'],);
-    } else {
-      return null;
-    }
-  }
-
-  void _getOG(Document doc, Map data, String param) {
-    var metaTag = doc.getElementsByTagName("meta").firstWhereOrNull((
-        meta) => meta.attributes['property'] == param);
-    if (metaTag != null) {
-      data[param] = metaTag.attributes['content'];
-    }
   }
 }
